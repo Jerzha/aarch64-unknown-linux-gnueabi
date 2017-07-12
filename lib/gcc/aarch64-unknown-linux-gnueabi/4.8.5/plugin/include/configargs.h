33ee07ce9f0a94f488103aca96d4a769a5288096
Jeffry Zhang <zhanghao@rock-chips.com>
2017-07-12 14:20:37 +0800

    for 4.8.5

---
+++ lib/gcc/aarch64-unknown-linux-gnueabi/4.8.5/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/Volumes/aarch64/.build/src/gcc-4.8.5/configure --build=x86_64-build_apple-darwin16.6.0 --host=x86_64-build_apple-darwin16.6.0 --target=aarch64-unknown-linux-gnueabi --prefix=/Volumes/aarch64/aarch64-unknown-linux-gnueabi --with-sysroot=/Volumes/aarch64/aarch64-unknown-linux-gnueabi/aarch64-unknown-linux-gnueabi/sysroot --enable-languages=c,c++ --with-pkgversion='crosstool-NG crosstool-ng-1.22.0' --enable-__cxa_atexit --disable-libmudflap --disable-libgomp --disable-libssp --disable-libquadmath --disable-libquadmath-support --disable-libsanitizer --with-gmp=/Volumes/aarch64/.build/aarch64-unknown-linux-gnueabi/buildtools --with-mpfr=/Volumes/aarch64/.build/aarch64-unknown-linux-gnueabi/buildtools --with-mpc=/Volumes/aarch64/.build/aarch64-unknown-linux-gnueabi/buildtools --with-isl=/Volumes/aarch64/.build/aarch64-unknown-linux-gnueabi/buildtools --with-cloog=/Volumes/aarch64/.build/aarch64-unknown-linux-gnueabi/buildtools --with-libelf=/Volumes/aarch64/.build/aarch64-unknown-linux-gnueabi/buildtools --enable-lto --enable-threads=posix --enable-target-optspace --enable-plugin --enable-gold --disable-nls --disable-multilib --with-local-prefix=/Volumes/aarch64/aarch64-unknown-linux-gnueabi/aarch64-unknown-linux-gnueabi/sysroot --enable-long-long";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
